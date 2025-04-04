6b45656273e50774952a20550a88601689108c32
Muhammad Bilal <62239033+bilalpirzada@users.noreply.github.com>
2025-04-04 18:39:07 +0500

    Add functionality for adding tasks and showing task on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,48 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [taskInput, setTaskInput] = useState("do homework"); 
+  let [taskInput, setTaskInput] = useState(""); 
+  let [tasksArray, setTasksArray] = useState([]);
 
 
   function handleInput(event){
     setTaskInput(event.target.value);
   }
 
+  function handleAddTask(){
+    if(taskInput){
+      setTasksArray((prevTasks)=>{return [...prevTasks,taskInput]});
+      setTaskInput(""); // clear task input after adding 
+    }
+    console.log(tasksArray);
+  }
+
   return <>
-  <h1 className=" text-green-400 mb-20">React Todo App</h1>
-  <input value={taskInput} onChange={handleInput} className="bg-amber-50 rounded-4xl px-10 py-5 text-gray-800 text-2xl"/>
-  <button className="ml-5 px-8 py-4 bg-green-600 rounded-4xl ">Add</button>
+  <h1 
+    className=" text-green-400 mb-20">
+    React Todo App
+  </h1>
+  <div>
+    <input 
+      value={taskInput} 
+      onChange={handleInput} 
+      className="bg-amber-50 rounded-4xl py-4 text-gray-800 w-100 px-6 text-xl"
+    />
+    <button 
+      className="ml-5 px-8 py-4 bg-green-600 rounded-4xl" 
+      onClick={handleAddTask}>
+        Add
+    </button>
+  </div>
+  
+  
+    <ol className='mt-10 flex justify-start'>
+    
+    {tasksArray.map((task,index)=>{
+       return <li key={index}>{task}</li>
+    })}
+    </ol>
+
   </>
 }
 
