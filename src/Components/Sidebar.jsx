74e07a19776427a98291bfde7579d9ee3593d0d7
Muhammad Bilal <62239033+bilalpirzada@users.noreply.github.com>
2025-09-10 13:47:24 +0500

    Modify text of sidebar items

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,29 +6,33 @@ import {
   ListItemPrefix,
 } from "@material-tailwind/react";
 
- import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Sidebar() {
   return (
     <Card className="h-[calc(100vh)] w-full max-w-[12rem] p-4  shadow-xl shadow-green-400 fixed top-0 left-0 rounded-[0px]">
       <div className="mb-2 p-4">
-        <Typography variant="h5" color="blue-gray">
+        <Typography variant="h5" color="blue-gray" className="">
           Sidebar
         </Typography>
       </div>
       <List className="">
-        <ListItem className="p-1">
-          <ListItemPrefix>
-            ✅
-          </ListItemPrefix>
-          <Link to='/' className='cursor-pointer no-underline active:text-white visited:text-white'>Todo App</Link>
-        </ListItem>
-        <ListItem className="p-1">
-          <ListItemPrefix>
-            🔥
-          </ListItemPrefix>
-           <Link to='/contributions' className='cursor-pointer no-underline active:text-white visited:text-white'>Streaks</Link>
-        </ListItem>
+        <Link to='/' className='cursor-pointer no-underline active:text-white visited:text-white text-sm '>
+          <ListItem className="p-1 ">
+            <ListItemPrefix>
+              ✅
+            </ListItemPrefix>
+            Todo App
+          </ListItem>
+        </Link>
+        <Link to='/contributions' className='cursor-pointer no-underline active:text-white visited:text-white text-sm'>
+          <ListItem className="p-1">
+            <ListItemPrefix>
+              🔥
+            </ListItemPrefix>
+            Streaks
+          </ListItem>
+        </Link>
       </List>
     </Card>
   );
